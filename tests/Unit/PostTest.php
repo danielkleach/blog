dd762f8baef97dffb2b9fc6e1190f61b3da12674
Daniel Leach <dleach@kimmel.com>
2017-08-09 13:22:38 -0400

    Initial commit.

---
+++ tests/Unit/PostTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        // Arrange
+        // Create 5 posts
+
+        // Act
+        // View the posts
+
+        // Assert
+        // See the posts
+    }
+}
