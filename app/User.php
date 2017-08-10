c91b4b2de7ddb7c05f0226677ce031cab71fef1d
Daniel Leach <dleach@kimmel.com>
2017-08-10 10:31:25 -0400

    Updating User model.

--- app/User.php
+++ app/User.php
@@ -10,21 +10,20 @@ class User extends Authenticatable
 {
     use Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /***************************************/
+    /*************** Methods ***************/
+    /***************************************/
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
