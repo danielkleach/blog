db9d3bb9cf9f618c46ffa022a82d1536bc994b44
Daniel Leach <dleach@kimmel.com>
2017-08-10 10:22:18 -0400

    Adding web routes.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::group(['prefix' => 'posts'], function () {
+
+    Route::get('/', ['as' => 'post.index', 'uses' => 'PostController@index']);
+    Route::post('/', ['as' => 'post.store', 'uses' => 'PostController@store']);
+
+    Route::group(['prefix' => '{postId}'], function () {
+
+        Route::get('/', ['as' => 'post.show', 'uses' => 'PostController@show']);
+        Route::patch('/', ['as' => 'post.update', 'uses' => 'PostController@update']);
+        Route::delete('/', ['as' => 'post.delete', 'uses' => 'PostController@destroy']);
+    });
 });
