0fc6d6226ca1785ed54ccc68b04ef19043aaaf4e
Daniel Leach <dleach@kimmel.com>
2017-08-16 16:03:46 -0400

    Adding relationships.

--- app/Subject.php
+++ app/Subject.php
@@ -10,4 +10,13 @@ class Subject extends Model
     use SoftDeletes;
 
     protected $guarded = [];
+
+    /***************************************/
+    /************ Relationships ************/
+    /***************************************/
+
+    public function post()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
