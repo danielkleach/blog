f2637b4bfc356beb26e575c0897127de89004f0f
Daniel Leach <dleach@kimmel.com>
2017-08-16 16:01:37 -0400

    Updating routes.

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,7 @@
 
 Auth::routes();
 
-Route::group(['prefix' => 'subjects'], function () {
-
-    Route::get('/', ['as' => 'subject.index', 'uses' => 'SubjectController@index']);
-    Route::post('/', ['as' => 'subject.store', 'uses' => 'SubjectController@store']);
-
-    Route::group(['prefix' => '{subjectId}'], function () {
-
-        Route::get('/', ['as' => 'subject.show', 'uses' => 'SubjectController@show']);
-        Route::patch('/', ['as' => 'subject.update', 'uses' => 'SubjectController@update']);
-        Route::delete('/', ['as' => 'subject.delete', 'uses' => 'SubjectController@destroy']);
-    });
-});
-
-Route::group(['prefix' => 'posts'], function () {
+Route::group(['prefix' => 'blog'], function () {
 
     Route::get('/', ['as' => 'post.index', 'uses' => 'PostController@index']);
     Route::post('/', ['as' => 'post.store', 'uses' => 'PostController@store']);
@@ -36,5 +23,18 @@ Route::group(['prefix' => 'posts'], function () {
         Route::get('/', ['as' => 'post.show', 'uses' => 'PostController@show']);
         Route::patch('/', ['as' => 'post.update', 'uses' => 'PostController@update']);
         Route::delete('/', ['as' => 'post.delete', 'uses' => 'PostController@destroy']);
+    });
+
+    Route::group(['prefix' => 'subjects'], function () {
+
+        Route::get('/', ['as' => 'subject.index', 'uses' => 'SubjectController@index']);
+        Route::post('/', ['as' => 'subject.store', 'uses' => 'SubjectController@store']);
+
+        Route::group(['prefix' => '{subjectId}'], function () {
+
+            Route::get('/', ['as' => 'subject.show', 'uses' => 'SubjectController@show']);
+            Route::patch('/', ['as' => 'subject.update', 'uses' => 'SubjectController@update']);
+            Route::delete('/', ['as' => 'subject.delete', 'uses' => 'SubjectController@destroy']);
+        });
     });
 });
