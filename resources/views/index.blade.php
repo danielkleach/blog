dbacec927fc12dd0afc05bd743b511df9e7e8c64
Daniel Leach <dleach@kimmel.com>
2017-08-24 16:12:08 -0400

    Updating homepage.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,21 @@
 @extends('layout.master')
 
 @section('content')
-    @foreach ($latestPosts as $post)
-        <div class="article-block">
-            <span class="subject"><a href="{{ route('subject.show', $post->subject->slug) }}">{{ $post->subject->name }}</a></span>
-            <h2 class="title"><a href="{{ route('post.show', $post->slug) }}">{{ $post->title }}</a></h2>
-            <span class="date">{{ $post->formatted_date }}</span>
-            <div class="content">
-                <p>{{ $post->content }}</p>
+    <main>
+        @foreach ($latestPosts as $post)
+            <div class="article-block">
+                <span class="subject"><a href="{{ route('subject.show', $post->subject->slug) }}">{{ $post->subject->name }}</a></span>
+                <h2 class="title"><a href="{{ route('post.show', $post->slug) }}">{{ $post->title }}</a></h2>
+                <span class="date">{{ $post->formatted_date }}</span>
+                <div class="content">
+                    <p>{{ $post->content }}</p>
+                </div>
+                <span class="read-more"><a href="{{ route('post.show', $post->slug) }}">Read More</a></span>
             </div>
-            <span class="read-more"><a href="{{ route('post.show', $post->slug) }}">Read More</a></span>
-        </div>
-    @endforeach
+        @endforeach
+    </main>
+@endsection
+
+@section('scripts')
+    <script src="/js/app.js"></script>
 @endsection
