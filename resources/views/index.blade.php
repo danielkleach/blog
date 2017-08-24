a2c2ce04b7e567b12f7651bb9136fafc30e2b316
Daniel Leach <dleach@kimmel.com>
2017-08-24 16:34:40 -0400

    Removing link from subject.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <main>
         @foreach ($latestPosts as $post)
             <div class="article-block">
-                <span class="subject"><a href="{{ route('subject.show', $post->subject->slug) }}">{{ $post->subject->name }}</a></span>
+                <span class="subject">{{ $post->subject->name }}</span>
                 <h2 class="title"><a href="{{ route('post.show', $post->slug) }}">{{ $post->title }}</a></h2>
                 <span class="date">{{ $post->formatted_date }}</span>
                 <div class="content">
