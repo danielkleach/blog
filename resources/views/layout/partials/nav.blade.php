5de65913b4d943391e2b7f87b4a609d41551a5ea
Daniel Leach <dleach@kimmel.com>
2017-09-20 10:28:07 -0400

    Adding Twitter link to nav.

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -2,6 +2,7 @@
     <div id="navbar">
         <ul class="nav">
             <li><a href="{{ route('home.index') }}">Home</a></li>
+            <li><a href="https://twitter.com/danielkleach">Twitter</a></li>
             <li><a href="https://github.com/danielkleach" target="_blank">Github</a></li>
             <li><a href="https://www.linkedin.com/in/daniel-leach-477a3847" target="_blank">LinkedIn</a></li>
         </ul>
